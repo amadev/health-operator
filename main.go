82c0e4787381f421190a621749081557777fbf4b
Andrey Volkov <avolkov@mirantis.com>
2020-09-24 17:31:02 +0300

    StatefulSet controller

--- main.go
+++ main.go
@@ -75,6 +75,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Health")
 		os.Exit(1)
 	}
+	if err = (&controllers.StatefulSetHealthReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("StatefulSetHealth"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "StatefulSetHealth")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
